fffc246f889cde04a06863e1ddef66ed60b668e0
Roo Reynolds <roo.reynolds@gmail.com>
2012-11-07 14:11:04 +0000

    changing last updated time to be time of build (rather than last commit) for now

--- compiling/compile.rb
+++ compiling/compile.rb
@@ -226,7 +226,8 @@ class Compile
     contents.gsub!(/{\/collapsed}/, "</div>")
     contents.gsub!(/{TIMESTAMP}/) {
       #TODO: CHANGE THIS DATE
-      date = GithubFetch.get_date_for_repo('alphagov', 'government-digital-strategy').to_time
+      # date = GithubFetch.get_date_for_repo('alphagov', 'government-digital-strategy').to_time
+      date = Time.now # for now...
       if folder
         "[#{date.stamp("1 Nov 2012 at 12:30 am")}](https://github.com/alphagov/government-digital-strategy-prerelease/commits/master/source/#{folder})"
       else
