6199c418c761af57bf1c7b38db2d167b3a0233a4
Jack Franklin <jack@jackfranklin.net>
2012-11-29 13:31:31 +0000

    fix script to pull in assisted digital following folder structure change

--- compiling/get_assisted_digital.rb
+++ compiling/get_assisted_digital.rb
@@ -12,10 +12,10 @@ puts "-> Cloning the Assisted Digital Prerelease to /tmp/get_external"
 Shell.execute('cd /tmp/get_external && git clone git@github.com:alphagov/assisted-digital-prerelease.git')
 
 puts "-> Removing old assisted-digital-prelease folder"
-Shell.execute("cd #{pwd} && rm -r /source/assisted-digital")
+Shell.execute("cd #{pwd} && rm -r /source/digital/assisted")
 
-puts "-> Copying /tmp/get_external/assisted-digital-prelease/source to #{pwd}/source/assisted-digital"
-FileUtils.cp_r("/tmp/get_external/assisted-digital-prerelease/source/assisted-digital", "#{pwd}/source")
+puts "-> Copying /tmp/get_external/assisted-digital-prelease/source to #{pwd}/source/"
+FileUtils.cp_r("/tmp/get_external/assisted-digital-prerelease/source/digital/assisted", "#{pwd}/source/digital")
 
 puts "-> Clearing out /tmp"
 Shell.execute("cd /tmp/get_external && rm -r assisted-digital-prerelease")
