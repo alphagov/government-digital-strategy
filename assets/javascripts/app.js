7c724a1cc0f34a21931c70cfb81b04d7b8450b32
Jack Franklin <jack@jackfranklin.net>
2012-12-03 10:36:22 +0000

    add V3 of MC and toggling of charts functionality

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -21,22 +21,14 @@ require(['modules/anchor_scrolling'], function(scrollToAnchor) {
 
 require(['modules/magna-charta.min'], function() {
   $(".horizontal-bar-chart").each(function(i, item) {
-    var opts = {
-      outOf: 70,
-      autoOutdent: true
-    }
-    if($(item).hasClass("mc-stacked")) {
-      opts.barPadding = 5;
-    } else if(!$(item).hasClass("mc-negative")) {
-      opts.barPadding = 2;
-    }
-
     // some special cases
-    $.magnaCharta($(item), opts);
+    $.magnaCharta($(item), {
+      outOf: 70,
+      autoOutdent: true,
+    });
   });
   $('.proportional.breakdown-chart tbody tr td:first-child').css('height', function(index) {
-    var height = +($(this).text().replace('%', '')) * 4;
-    return height;
+    return +($(this).text().replace('%', '')) * 4;
   });
 });
 
