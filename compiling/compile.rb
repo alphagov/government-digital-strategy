4b207faae036c6762daaf3a5012e7ecd51c71d92
Jack Franklin <jack@jackfranklin.net>
2012-12-05 15:55:21 +0000

    refactor compiling code into more individual classes - based on the single resposibility principle

--- compiling/compile.rb
+++ compiling/compile.rb
@@ -10,6 +10,7 @@ require_relative "./utils.rb"
 require_relative "./compileutils.rb"
 require_relative "./compilesass.rb"
 require_relative "./xmlfiles.rb"
+require_relative "./processcontents.rb"
 
 
 class Compile
@@ -74,7 +75,7 @@ class Compile
       @f.indent {
         @f.display_lines("Compiling #{mj}")
       }
-      self.compile_single_markdown_joined mj, self.process_template_partial( template.clone )
+      self.compile_single_markdown_joined mj, self.find_compile_partial(template.clone)
     end
   end
 
@@ -120,104 +121,14 @@ class Compile
         File.open("temp/#{folder}/markdown_joined.md", "a") do |open|
           contents = file.read
           if item[0..2] == "00-"
-            contents = "<div class='document-title'>\n\n#{self.pre_process contents, folder}\n\n</div>"
+            contents = "<div class='document-title'>\n\n#{ProcessContents.process contents, folder}\n\n</div>"
           else
-            contents = "<div class='section'>\n\n#{self.pre_process contents, folder}\n\n</div>"
+            contents = "<div class='section'>\n\n#{ProcessContents.process contents, folder}\n\n</div>"
           end
           open.puts contents
         end
       end
     end
-  end
-
-
-
-
-  # pre-process the Markdown before compilation to deal with our extra stuff
-  def self.pre_process(contents, folder = "")
-    contents.force_encoding("UTF-8")
-
-    # sort out partials first so everything else can use them fine
-    contents.gsub!(/{include\s*(.+)\.(.+)}/) { |match|
-      @f.indent {
-        @f.display_line("Replacing partial #{match}")
-      }
-      CompileUtils.get_partial_content $1, $2
-    }
-
-    # match section title headlines
-    # these are either Annex ones in the form ##Annex 1 - Foo Bar
-    # or section titiles in the form: ##09 Foo Bar
-    # this regex matches both
-    contents.gsub!(/##(?:Annex\s)?([0-9]+) (?:-)?(.+)/) { |match|
-      number = $1
-      if match.include? "Annex"
-        number = "Annex #{number}"
-      end
-      title = $2
-      slug = $2.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-      "{::options auto_ids='false' /}\n\n##<span class='title-index'>#{number}</span> <span class='title-text'>#{title.strip}</span>\n{: .section-title ##{slug}}\n{::options auto_ids='true' /}"
-    }
-
-    #add links to figures
-    contents.gsub!(/^(Figure .+?){: \.fig (#fig-.+?)}$/m) { |match|
-      "<a href='#{$2}' class='figure-permalink' title='Right click to copy a link to this figure'>Link to this</a> \n #{match}"
-    }
-
-    # some shortcuts to adding Kramdown classes
-    contents.gsub!(/{pull}/) { "{: .pull}" }
-    contents.gsub!(/{big-pull}/) { "{: .big-pull}" }
-    contents.gsub!(/{fig}/) { "{: .fig}" }
-
-    # lets escape some unicode characters
-    contents.gsub!(/([£])/, '&pound;')
-    contents.gsub!(/([€])/, '&euro;')
-    contents.gsub!(/[”“]/, '"')
-    contents.gsub!(/[‘’]/, "'")
-    contents.gsub!(/[…]/, "...")
-    contents.gsub!(/[–]/, "--")
-
-    # lets us do ^5^ => <sup>5</sup>
-    contents.gsub!(/\^(.+)\^/) { "<sup>#{$1}</sup>" }
-    # this is some weird non-breaking-space unicode that was causing us problems with random chars appearing.
-    contents.gsub!(/\u00a0/, " ")
-    contents.gsub!(/{page-break}/, "<div class='page-break'></div>")
-    contents.gsub!(/{collapsed}/, "<div class='theme'>")
-    contents.gsub!(/{\/collapsed}/, "</div>")
-    contents.gsub!(/{TIMESTAMP}/) {
-      date = Shell.execute("git log -1 --pretty=format:'%ad%x09' source/#{folder}").stdout
-      # if we dont get a date, just go for the current time.
-      date = (date == "" ? Time.now : DateTime.parse(date))
-      "[#{date.stamp("1 Nov 2012 at 12:30 am")}](https://github.com/alphagov/government-digital-strategy/commits/master/source/#{folder})"
-    }
-    # shortcut for PDF linking
-    contents.gsub!(/{PDF=(.+)}/) {
-      "[PDF format](#{$1})"
-    }
-    contents.gsub!(/###theme(.+)/) { |match|
-      m = $1
-      m.strip!
-      "####{m}\n {: .theme-head}"
-    }
-
-    # shortcut that allows for HTML element inputs
-    # eg {div .foo} => <div class="foo">
-    contents.gsub!(/{(\w+) .(.+)}/) {
-      "<#{$1} class='#{$2}'>"
-    }
-
-    # closing tags
-    # {/div} => </div>
-    contents.gsub!(/{\/(\w+)}/) {
-      "</#{$1}>"
-    }
-
-    #deal with action headings
-    contents.gsub!(/####Action ([0-9]+): (.+)/) {
-      "<h4 id='action-#{$1}' class='section-title'><span class='title-index'><span>Action </span> #{$1}</span><span class='title-text'>#{$2}</span></h4>"
-    }
-
-    contents
   end
 
   # replace HTML templates wihin a file
@@ -243,7 +154,7 @@ class Compile
         File.exists?("source/#{parent_path}/meta.html") ? Utils.read_from_file("source/#{parent_path}/meta.html") : ""
       }
       # deal with any partials that might exist in the template
-      template_contents = self.process_template_partial(template_contents)
+      template_contents = self.find_compile_partial(template_contents)
 
       # save to file
       File.open("built/#{file}", "w") do |html_file|
@@ -266,18 +177,13 @@ class Compile
     self.find_compile_partial(file_contents)
   end
 
-  # takes the contents of a template, and compiles the partials within
-  def self.process_template_partial(template_contents)
-    self.find_compile_partial(template_contents)
-  end
-
   # takes contents, finds the partials, and compile them and put the content in place
   def self.find_compile_partial(file_contents)
     file_contents.gsub!(/{include\s*(.+)\.(.+)}/) { |match|
       partial_contents = CompileUtils.get_partial_content($1, $2)
       if $2 == "md"
         # markdown
-        partial_contents = Kramdown::Document.new(self.pre_process(partial_contents)).to_html
+        partial_contents = Kramdown::Document.new(ProcessContents.process(partial_contents)).to_html
       end
       partial_contents
     }
