6c20153413d375c76e63288ea6fb12278d2e6054
Jack Franklin <jack@jackfranklin.net>
2012-12-12 13:34:03 +0000

    tweak get_content to make it work on our preview server

--- compiling/get_content.rb
+++ compiling/get_content.rb
@@ -18,21 +18,26 @@ end
 @f.indent {
   @f.display_line("Cloning to /tmp/get_external")
 }
-if Shell.execute('cat ~/.ssh/config').success?
+
+on_server = Shell.execute('cat ~/.ssh/config').success?
+if on_server
   # on a server, so use the deploy key
   Shell.execute('cd /tmp/get_external && git clone git@github-gds-content:alphagov/government-digital-strategy-content.git')
 else
   Shell.execute('cd /tmp/get_external && git clone git@github.com:alphagov/government-digital-strategy-content.git')
 end
 
-# get current branch
-b = `git branch`.split("\n").delete_if { |i| i.strip.chars.first != "*" }
-branch = b.first.gsub("* ","")
+# on the server we always want to use the add-departmental-strategies branch
+unless on_server
+  # get current branch
+  b = `git branch`.split("\n").delete_if { |i| i.strip.chars.first != "*" }
+  branch = b.first.gsub("* ","")
 
-@f.indent {
-  @f.display_line("Using branch #{branch}")
-}
-Shell.execute("cd /tmp/get_external/government-digital-strategy-content && git pull origin && git checkout #{branch}")
+  @f.indent {
+    @f.display_line("Using branch #{branch}")
+  }
+  Shell.execute("cd /tmp/get_external/government-digital-strategy-content && git pull origin && git checkout #{branch}")
+end
 
 
 
