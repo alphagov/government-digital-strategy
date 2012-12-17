36c2b9823041f1ae1acbe0eca9b054853ad447b8
Jack Franklin <jack@jackfranklin.net>
2012-12-17 15:23:36 +0000

    Add a comment to build script to clarify why we're passing the argument through

--- local-build.sh
+++ local-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ruby compiling/get_content.rb
+
+# we pass through $1 as if you pass through a parameter
+# then it does the deploy steps as well (minifying)
 ruby compiling/build.rb $1 --trace
 
