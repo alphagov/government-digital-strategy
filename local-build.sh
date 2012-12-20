4e0c68e3feb7f589d3e91030d1bf8305907cb8c4
Jack Franklin <jack@jackfranklin.net>
2012-12-20 18:27:30 +0000

    update local build script

--- local-build.sh
+++ local-build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-ruby compiling/get_content.rb
-
 # we pass through $1 as if you pass through a parameter
 # then it does the deploy steps as well (minifying)
 ruby compiling/build.rb $1 --trace
